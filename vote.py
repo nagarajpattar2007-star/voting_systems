b4bd7cd0ffd2b44391d1f75f00afabc2e264d13b
nagarajpattar2007-star <nagarajpattar2007@gmail.com>
2026-09-12 12:04:26 +0530

    Fix typo in voting eligibility message

--- vote.py
+++ vote.py
@@ -3,4 +3,4 @@ age = int(input("Enter your age: "))
 if age <= 18:
     print("You are eligible for voting")
 else:
-    print("You are not eligible for voting")
+    print("You are not eligibles for voting")
